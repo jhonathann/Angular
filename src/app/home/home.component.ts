1076796c0af0bf70e59bde1dfc6bd721696fd23a
Jhonathann A. Gómez V <jhagomezve@unal.edu.co>
2022-09-12 10:17:40 -0500

    RxJS Part 1

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -21,9 +21,9 @@ export class HomeComponent implements OnInit {
   ) { }
 
   ngOnInit() {
-    this.dishService.getFeaturedDish().then(dish=>this.dish=dish);
-    this.promotionService.getFeaturedPromotion().then(promotion=>this.promotion=promotion);
-    this.LeaderService.getFeaturedLeader().then(leader=>this.leader=leader);
+    this.dishService.getFeaturedDish().subscribe(dish=>this.dish=dish);
+    this.promotionService.getFeaturedPromotion().subscribe(promotion=>this.promotion=promotion);
+    this.LeaderService.getFeaturedLeader().subscribe(leader=>this.leader=leader);
   }
 
 }
